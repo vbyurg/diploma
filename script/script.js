58721af45fddadf9f2d53ebe675d39e116375f79
Viktor Byurg <v.byurg@gmail.com>
2022-06-15 22:47:41 +0300

    add new pages

--- script/script.js
+++ script/script.js
@@ -34,3 +34,58 @@ function showSlides(n) {
 
     slides[slideIndex - 1].style.display = "block";
 }
+
+
+// input tel validation
+document.querySelector(".phone").addEventListener("keyup", function(){
+    this.value = this.value.replace(/[^\d]/g, "");
+});
+
+
+// ===========================================================
+// email validation
+// document.querySelector(function() {
+//     document.querySelector(".btn_submit").addEventListener("click", validate);
+   
+    // Validate email
+    // function validateEmail(email) {
+    //   let i = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    //   return i.test(String(email).toLowerCase());
+    // }
+     
+    // send form
+    // function sendForm() {
+    //   document.querySelector(".error").text("Form sending").fadeIn();
+    // }
+   
+    // validate email and send form after success validation
+//     function validate() {
+//       let email = document.querySelector(".email").value;
+//       let $error = document.querySelector(".error");
+//       $error.text("");
+   
+//       if (validateEmail(email)) {
+//         $error.fadeOut();
+//         sendForm();
+//       } else {
+//         $error.fadeIn();
+//         $error.text(email + " неверно введён email");
+//       }
+//       return false;
+//     }
+//   });
+
+
+
+
+//   <form class="form" action="javascript:void(0)">
+//   <div class="form-row">
+//     <div class="form-col">
+//       <input type="text" class="email" placeholder="E-mail">
+//       <span class="error"></span>
+//     </div>
+//   </div>
+//   <button type="submit" class="btn-submit">Get Discount</button>
+// </form>
+
+  // ===========================================================
