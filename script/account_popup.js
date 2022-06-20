afed5d07da5e884816bed0ab88c82072d66e30b9
Viktor Byurg <v.byurg@gmail.com>
2022-06-20 13:25:54 +0300

    changes

--- script/account_popup.js
+++ script/account_popup.js
@@ -1,16 +1,43 @@
 // Модальное окно
 
-// открыть
+
 $('.js-button-campaign').click(function() { 
-	
 	$('.js-overlay-campaign').fadeIn();
 	$('.js-overlay-campaign').addClass('disabled');
 });
 
-// закрыть
+
 $('.js-close-campaign').click(function() { 
 	$('.js-overlay-campaign').fadeOut();
 	
 });
 
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign').fadeOut();
+		
+	}
+});
 
+
+// acc reg
+
+$('.js-button-reg').click(function() { 
+	$('.js-overlay-reg').fadeIn();
+	$('.js-overlay-reg').addClass('disabled');
+});
+
+
+$('.js-close-campaign-reg').click(function() { 
+	$('.js-overlay-reg').fadeOut();
+	
+});
+
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign-reg');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-reg').fadeOut();
+		
+	}
+});
